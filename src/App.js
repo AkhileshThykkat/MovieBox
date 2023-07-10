4645ce5724464e61ba72f3adffa523938e273e51
Akhilesh Thykkat <akhileshthykkat843@gmail.com>
2023-07-10 22:48:18 +0530

    On branch master Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	deleted:    public/favicon.ico 	new file:   public/favicon.png 	modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/MovieCard.jsx 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	new file:   src/search.svg 	deleted:    src/setupTests.js 	new file:   tailwind.config.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import MovieCard from "./MovieCard";
+import SearchIcon from "./search.svg";
 
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=c71f08e0";
+//here we are using an API so we have to load this api at the time where the page first loaded
+// so we are going to use the useEffect hook for this
+// so first step in building this api driven app is to use a useEffect hook
+// we have to import useEffect hooke from react library
+// now lets just understand what is  a useeffect hook is
+// so basically it will have a call back function as well as an empty dependency array
+//we have to define the function that calls the data from the api
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("Batman");
+  // search movies is the function which is used to fetch the movies
+  const searchMovies = async (Title) => {
+    const response = await fetch(`${API_URL}&s=${Title}`);
+    const data = await response.json();
+    console.log(data.Search);
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    //over here we will be needing a function which gets data from the api
+    searchMovies(searchTerm);
+  }, [searchTerm]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieBox</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies of that sort</h2>
+        </div>
+      )}
     </div>
   );
 }
